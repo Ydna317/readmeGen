d88c812e1b1e5e7350036e9857c58989f0ca350f
Andy Nguyen <Ydna317@gmail.com>
2021-03-09 21:00:31 -0800

    finalizing product.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# readmeGen
+Nethroi, Apex of Death.
+  ## Description
+   Nethroi is a 5/5  Nightmare Cat Beast with deathtouch and lifelink. If mutated, Nethroi can reanimate creatures from the graveyard with a cumulative power of up to ten. Two colorless and Abzan for cast. Four colorless and Abzan for mutate. 
+  ## Table of Contents 
+  * [Installation](#install)
+  * [Usage](#user)
+  * [Contributing](#contributing)
+  * [Tests](#test)
+  * [Questions](#questions)
+  ## Installation
+  To install necessary dependencies, run the following command:
+  ```
+  NPM INSTALL
+  ```
+  ## Usage
+  Please use this repo for all things commander related.
+  ## Contributing
+  Please comment out what you've added, removed, or changed.
+  ## Tests
+  To run tests, run the following command:
+  ```
+  NPM TEST
+  ```
+  ## Questions
+  If you have any questions about the repo, please send me an email at Ydna317@gmail.com.
--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-// GIVEN a command-line application that accepts user input
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
-// array of questions for user
 const questions = [
-    // WHEN I am prompted for information about my application repository
-    // WHEN I enter my project title
     {
         type: "input",
         name: "project",
         message: "What do we call your project?"
     },
-    // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
     {
         type: "input",
         name: "description",
         message: "Please give a detailed description of your project."
     },
-    // WHEN I choose a license for my application from a list of options
     {
         type: "list",
         name: "license",
         message: "What kind of license will your project be under?",
         choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
     },
-    // WHEN I enter my GitHub username
     {
         type: "input",
         name: "username",
         message: "What is your username for GitHub?"
     },
-    // WHEN I enter my email address
     {
         type: "input",
         name: "email",
@@ -60,11 +51,9 @@ const questions = [
         message: "What does the user need to know about contributing to the repo?",
     }
 ];
-// function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
   }
-// function to initialize program  
 function init() {
     inquirer.prompt(questions)
     .then((inquirerResponses) => {
@@ -72,6 +61,34 @@ function init() {
       writeToFile("README.md", generateMarkdown({...inquirerResponses}));
     })
   }
-// function call to initialize program
 init();
 
+function generateMarkdown(data) {
+    return `${data.project}
+  ## Description
+  ${data.description}
+  ## Table of Contents 
+  * [Installation](#install)
+  * [Usage](#user)
+  * [Contributing](#contributing)
+  * [Tests](#test)
+  * [Questions](#questions)
+  ## Installation
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${data.install}
+  \`\`\`
+  ## Usage
+  ${data.user}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  To run tests, run the following command:
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  ## Questions
+  If you have any questions about the repo, please send me an email at ${data.email}.`
+  ;}
+  module.exports = generateMarkdown;
+  
